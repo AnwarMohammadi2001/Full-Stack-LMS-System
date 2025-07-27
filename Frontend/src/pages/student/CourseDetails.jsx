4442484efefb402258e67d153528b9a7716af8ad
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-07-27 10:39:36 +0430

    working on CourseDetails page

--- Frontend/src/pages/student/CourseDetails.jsx
+++ Frontend/src/pages/student/CourseDetails.jsx
@@ -2,11 +2,25 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
 import assets from "../../assets/assets";
+import humanizeDuration from "humanize-duration";
 
 const CourseDetails = () => {
   const { id } = useParams(); // گرفتن id از URL
-  const { allCourses, calculateRating } = useContext(AppContext); // گرفتن لیست همه دوره‌ها از context
+  const [openSection, setOpenSection] = useState({});
+  const {
+    allCourses,
+    calculateRating,
+    calculateNoOFLectures,
+    calculateCourseDuration,
+    calculateChapterTime,
+  } = useContext(AppContext); // گرفتن لیست همه دوره‌ها از context
   const [courseData, setCourseData] = useState(null); // تعریف state برای نگهداری داده‌های دوره
+  const toggleSection = (index) => {
+    setOpenSection((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
 
   useEffect(() => {
     // یافتن دوره مورد نظر بر اساس id از لیست allCourses
@@ -65,6 +79,72 @@ const CourseDetails = () => {
         <p className="text-sm">
           Course By <span className="text-blue-600">Creat</span>
         </p>
+        <div>
+          <p className="text-xl font-semibold">Course Structure</p>
+          <div>
+            {courseData.courseContent.map((chapter, index) => (
+              <div
+                key={index}
+                className="border border-gray-300 bg-white mb-2 rounded"
+              >
+                <div
+                  className="flex items-center justify-between px-4 py-3 cursor-pointer select-none "
+                  onClick={() => toggleSection(index)}
+                >
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={assets.down_arrow_icon}
+                      alt=" arrow icon"
+                      className={`transition transform ${
+                        openSection[index] ? "rotate-180" : ""
+                      } `}
+                    />
+                    <p className="font-medium md:text-base text-sm">
+                      {chapter.chapterTitle}
+                    </p>
+                  </div>
+                  <p className="text-sm md:text-default">
+                    {chapter.chapterContent.length} - Lectures{" "}
+                    {calculateChapterTime(chapter)}
+                  </p>
+                </div>
+                <div
+                  className={`overflow-hidden transition-all duration-300 ${
+                    openSection[index] ? "max-h-96" : "max-h-0"
+                  }`}
+                >
+                  <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                    {chapter.chapterContent.map((lecture, i) => (
+                      <li key={i} className="flex items-start gap-2 py-1">
+                        <img
+                          src={assets.play_icon}
+                          alt=""
+                          className="w-4 h-4 mt-1"
+                        />
+                        <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                          <p>{lecture.lectureTitle}</p>
+                          <div className="flex gap-2 items-center">
+                            {lecture.isPreviewFree && (
+                              <p className="text-blue-500 cursor-pointer">
+                                Preview
+                              </p>
+                            )}
+                            <p>
+                              {humanizeDuration(
+                                lecture.lectureDuration * 60 * 1000,
+                                { units: ["h", "m"] }
+                              )}
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       {/* right column */}
       <div></div>
