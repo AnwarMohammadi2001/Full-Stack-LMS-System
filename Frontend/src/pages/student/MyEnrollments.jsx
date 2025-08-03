0ae9c0e7ecbd47b589e26b6e34b298905ee241a1
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-08-03 10:22:00 +0430

    new changs

--- Frontend/src/pages/student/MyEnrollments.jsx
+++ Frontend/src/pages/student/MyEnrollments.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const MyEnrollments = () => {
-  return <div>MyEnrollmets</div>;
+  return (
+    <>
+      <div className="md:px-36 px-8 pt-10">
+        <h1 className="text-2xl font-semibold">My Enrollmets</h1>
+        <table className="">
+          <thead className="">
+            <tr className="md:table-auto table-fixed w-full overflow-hidden border mt-10">
+              <th className="px-4 py-3 font-semibold truncate ">Course</th>
+              <th className="px-4 py-3 font-semibold truncate ">Duration</th>
+              <th className="px-4 py-3 font-semibold truncate ">Completed</th>
+              <th className="px-4 py-3 font-semibold truncate ">Status</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default MyEnrollments;
